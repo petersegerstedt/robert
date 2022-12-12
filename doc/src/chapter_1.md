1fa5117aeb701ed92261bd49cc54338451faa139
Peter Segerstedt <petersegerstedt@users.noreply.github.com>
2022-12-12 21:46:00 +0100

    experiments using qrcodes in corners of canvas

--- doc/src/chapter_1.md
+++ doc/src/chapter_1.md
@@ -1,1 +1,17 @@
 # Chapter 1
+
+## Raspberry OpenCV
+
+https://pimylifeup.com/raspberry-pi-opencv/
+```
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D ENABLE_NEON=ON \
+    -D ENABLE_VFPV3=ON \
+    -D BUILD_TESTS=OFF \
+    -D INSTALL_PYTHON_EXAMPLES=OFF \
+    -D OPENCV_ENABLE_NONFREE=ON \
+    -D CMAKE_SHARED_LINKER_FLAGS=-latomic \
+    -D OPENCV_PYTHON_INSTALL_PATH=lib/python3.7/ \
+    -D BUILD_EXAMPLES=OFF ..
+```
--- python/__main__.py
+++ python/__main__.py
@@ -1,3 +1,5 @@
+
+
 def read_qr_code(filename):
     """Read an image and read the QR code.
     
@@ -9,10 +11,80 @@ def read_qr_code(filename):
     """
     import cv2 as cv
     import numpy as np
-    img = cv.imread(filename, cv.IMREAD_GRAYSCALE)
+    W = 1920
+    H = 1080
+    dst = dict(zip(('2', '3', '1', 'ul'), np.array([[[   2.    ,  886.    ],
+        [ 192.    ,  886.    ],
+        [ 192.    , 1076.    ],
+        [   2.    , 1076.    ]],
+
+       [[1724.    ,  885.    ],
+        [1913.9999,  885.    ],
+        [1913.9999, 1076.    ],
+        [1724.    , 1076.    ]],
+
+       [[1727.    ,    3.    ],
+        [1916.9999,    3.    ],
+        [1916.9999,  193.    ],
+        [1727.    ,  193.    ]],
+
+       [[   2.    ,    2.    ],
+        [ 192.    ,    2.    ],
+        [ 192.    ,  192.    ],
+        [   2.    ,  192.    ]]], dtype=np.float32)))
+    img = cv.imread(filename)
     detect = cv.QRCodeDetector()
+    status, data, pts, _ = detect.detectAndDecodeMulti(img)
+    print(data)
+
+    if status:
+        matched = [(k,src,dst[k]) for k,src in zip(data, pts) if k in dst]
+        if len(matched):
+            #pts1 = np.vstack([matched[0][1][:3],matched[1][1][0]])
+            #pts2 = np.vstack([matched[0][2][:3],matched[1][2][0]])
+            pts1 = np.vstack([pts for _,pts,__ in matched])
+            pts2 = np.vstack([pts for _,__,pts in matched])
+            matrix, _ = cv.findHomography(pts1, pts2)
+            result = cv.warpPerspective(img, matrix, (W, H))
+            roi = result[181:181+713, 176:176+1561]
+            roi = cv.flip(roi, 0) # flip vertially
+            roi = cv.flip(roi, 1) # flip horisontally
+            cv.imwrite(filename + '.roi.jpg', roi)
+            status2, data2, pts22, _ = detect.detectAndDecodeMulti(result)
+            print (roi.shape)
+            '''
+            print (matrix)
+            pts1 = matched[1][1]
+            pts2 = matched[1][2]
+            matrix = cv.getPerspectiveTransform(pts1, pts2)
+            print (matrix)
+            176 181 1561 713
+            cropped_image = img[80:280, 150:330]
+ 
+# Display cropped image
+cv2.imshow("cropped", cropped_image)
+ 
+# Save the cropped image
+cv2.imwrite("Cropped Image.jpg", cropped_image)
+            '''
+            #blurred = cv.GaussianBlur(roi, (31, 31), 0)
+            blurred = cv.blur(roi, (39, 39))
+            grain_extracted = roi - blurred + 128
+            #hist = cv.calcHist([grain_extracted],[0],None,[256],[0,256])
+            from matplotlib import pyplot as plt
+            color = ('b','g','r')
+            for i,col in enumerate(color):
+                histr = cv.calcHist([grain_extracted],[i],None,[256],[0,256])
+                plt.plot(histr,color = col)
+                plt.xlim([0,256])
+            plt.savefig(filename + '.roi_histogram.png')
+            cv.imwrite(filename + '.roi_grain_extract.jpg', grain_extracted)
+            #cv.namedWindow('image', cv.WINDOW_AUTOSIZE)
+            #cv.imshow('image', grain_extracted)
+            #cv.waitKey()
+    return
     value, pts1, straight_qrcode = detect.detectAndDecode(img)
-    print(value)
+    print(value, len(pts1))
     print(pts1[0])
     colors = [(255, 0, 0),(0, 255, 0),(0, 0, 255),(0, 255, 255)]
     for i, xy in enumerate(pts1[0]):
@@ -31,7 +103,12 @@ def read_qr_code(filename):
     minHessian = 400
 detector = cv.xfeatures2d_SURF.create(hessianThreshold=minHessian)
 keypoints = detector.detect(src)
+
+
+
     '''
+
+
     qr_size = 60
     w,h = (800, 800)
     margin = 80
